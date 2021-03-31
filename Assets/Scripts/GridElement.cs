faaf4ea05053103efa16f8c04a42b3f407ae2300
Serge Shevtsov <serge.shautsou@gmail.com>
2021-03-31 20:46:45 +0300

    Done!

---
+++ Assets/Scripts/GridElement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridElement : MonoBehaviour
+{
+    public int gridID;
+    public bool isOccupies;
+
+    //TODO: Which building is connected
+
+    private void Awake()
+    {
+        // show connected building
+    }
+}
